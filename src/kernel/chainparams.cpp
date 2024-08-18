962b6da80efb844c14f0498b54908bd403d96b54
traaitt | TRRXITTE Int., incorporate <traaitt@icloud.com>
2024-08-18 23:07:30 +0200

    /

--- src/kernel/chainparams.cpp
+++ src/kernel/chainparams.cpp
@@ -79,10 +79,10 @@ public:
             uint256S("0x00000000000002dc756eebf4f49723ed8d30cc28a5f108eb94b1ba88ac4f9c22"), SCRIPT_VERIFY_NONE);
         consensus.script_flag_exceptions.emplace( // Taproot exception
             uint256S("0x0000000000000000000f14c35b2d841e986ab5441de8c585d5ffe55ea1e395ad"), SCRIPT_VERIFY_P2SH | SCRIPT_VERIFY_WITNESS);
-        consensus.BIP34Height = 0;
+        consensus.BIP34Height = 1;
         consensus.BIP34Hash = uint256S("");
-        consensus.BIP65Height = 0; // 000000000000000004c2b624ed5d7756c508d90fd0da2c7c679febfa6c4735f0
-        consensus.BIP66Height = 0; // 00000000000000000379eaa19dce8c9b722d46ae6a57c2f1a988119488b50931
+        consensus.BIP65Height = 1; // 000000000000000004c2b624ed5d7756c508d90fd0da2c7c679febfa6c4735f0
+        consensus.BIP66Height = 1; // 00000000000000000379eaa19dce8c9b722d46ae6a57c2f1a988119488b50931
         consensus.CSVHeight = 0; // 000000000000000004a1b34462cb8aeebd5799177f7a29cf28f2d1961716b5b5
         consensus.SegwitHeight = 0; // 0000000000000000001c8018d9cb3b742ef25114f27563e3fc4a1902167f9893
         consensus.MinBIP9WarningHeight = 0; // segwit activation height + miner confirmation window
@@ -121,10 +121,10 @@ public:
         m_assumed_blockchain_size = 100;
         m_assumed_chain_state_size = 1;
 
-        genesis = CreateGenesisBlock(1724001017, 1899045113, 0x1d00ffff, 1, 50 * COIN);
+        genesis = CreateGenesisBlock(1724001017, 2288135342, 0x1d00ffff, 1, 50 * COIN);
         consensus.hashGenesisBlock = genesis.GetHash();
-        assert(consensus.hashGenesisBlock == uint256S("0x00000000f2b64896e100331f847828e27161a5fd39639b3da0b2c7d982f7a9d5"));
-        assert(genesis.hashMerkleRoot == uint256S("0x94dccb8edf482a61552e6102e0622e32c99087f8f5f2d3406df2a4eddbc83add"));
+        assert(consensus.hashGenesisBlock == uint256S("0x000000005aa70dd580fe107daa3f3b599557e88b316210a21f137ffe93eb1c89"));
+        assert(genesis.hashMerkleRoot == uint256S("0x4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b"));
 
         // Note that of those which support the service bits prefix, most only support a subset of
         // possible options.
